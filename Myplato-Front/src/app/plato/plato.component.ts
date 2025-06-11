9ad14b830a9cc6600231cb8e9899185b2b8800af
TREYCYBRY <2022210212@udh.edu.pe>
2025-06-11 00:11:27 -0500

    codigo basico

---
+++ Myplato-Front/src/app/plato/plato.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plato',
+  standalone: false,
+  templateUrl: './plato.component.html',
+  styleUrl: './plato.component.css'
+})
+export class PlatoComponent {
+
+}
