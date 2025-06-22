fb76f158b7072577390bf8fd694ec0e7a9baa4a4
TREYCYBRY <2022210212@udh.edu.pe>
2025-06-22 17:22:55 -0500

    Barra de inicio

---
+++ Myplato-Front/proyectPlato/.angular/cache/19.2.15/proyectPlato/vite/deps/primeng_config.js
@@ -0,0 +1,18 @@
+import {
+  PRIME_NG_CONFIG,
+  PrimeNG,
+  ThemeProvider,
+  providePrimeNG
+} from "./chunk-RJBTDME5.js";
+import "./chunk-M23OP5QM.js";
+import "./chunk-6RMOQLDJ.js";
+import "./chunk-UNCPHW44.js";
+import "./chunk-2NCRVGVE.js";
+import "./chunk-HM5YLMWO.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  PRIME_NG_CONFIG,
+  PrimeNG,
+  ThemeProvider,
+  providePrimeNG
+};
