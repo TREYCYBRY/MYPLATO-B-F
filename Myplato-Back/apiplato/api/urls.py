0298b749cf80d79b329ae630eb1b05aaabb616cd
TREYCYBRY <2022210212@udh.edu.pe>
2025-06-29 01:45:12 -0500

    ext

--- Myplato-Back/apiplato/api/urls.py
+++ Myplato-Back/apiplato/api/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 from . import views
-
 router = DefaultRouter()
 router.register('unidadDeMedida', views.UnidadMedidaViewSet)
 router.register('categoriaExtra', views.CategoriaExtraViewSet)
@@ -25,8 +24,6 @@ router.register('extraPlato', views.ExtraPlatoViewSet)
 router.register('almacen', views.AlmacenViewSet)
 router.register('stockComida', views.StockComidaViewSet)
 router.register('stockBebida', views.StockBebidaViewSet)
-
-
 urlpatterns = [
     path('',include(router.urls)),
     path('login/',views.LoginView.as_view(),name='login')
